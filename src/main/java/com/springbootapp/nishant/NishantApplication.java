ce5533c708275956043f262dfcb0a9801e30cd75
Nishant Madan <nishant.madan@xebia.com>
2019-08-12 10:17:03 +0530

    All files added

---
+++ src/main/java/com/springbootapp/nishant/NishantApplication.java
@@ -0,0 +1,13 @@
+package com.springbootapp.nishant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NishantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NishantApplication.class, args);
+	}
+
+}
